dcb15ec8a98ed2ecf90acf934caa459f1e8289ae
phillipsk <UTI88yup99[]>
2016-01-04 01:39:56 -0500

    first commit

---
+++ getchar.c
@@ -0,0 +1,13 @@
+//
+// Created by Tech_Ministry on 1/4/16.
+//
+
+#include	<stdio.h>
+
+main()
+{
+    int	c;
+
+    while ( (c = getchar()) != EOF )
+        printf("and the next char is %c\n", c);
+}
