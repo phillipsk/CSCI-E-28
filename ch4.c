8aa9d9d4c054926790f52e2115807f631ce56f3e
phillipsk <UTI88yup99[]>
2016-01-06 01:20:03 -0500

    following C book finished ch4,  ch5 q3

--- ch4.c
+++ ch4.c
@@ -1,33 +1,20 @@
 //
 // Created by Kevin Phillips on 1/5/16.
+// Following Programming in C by Stephen G Kochkan in preparation for CSCI E-28
 //
 
 #include <stdio.h>
 #include <math.h>
-//#include <conio.h>
-
-int main (void)
-{
-celsiusToF();
-polynomial();
-    printf("\n");
-next_multiple();
-////    getch();
-//    return 0;
-}
 
 int next_multiple(void){
     int i, j;
 
-
     i = 365 ,j = 7 ;
-    printf ("i = %i, j = %i, Next Largest Multiple = %i\n\n",i,j,i + j - i % j);
-
+    printf ("i = %i, j = %i, Next Largest Multiple = %i\n",i,j,i + j - i % j);
     i = 12258 ,j = 23 ;
-    printf ("i = %i, j = %i, Next Largest Multiple = %i\n\n",i,j,i + j - i % j);
-
+    printf ("i = %i, j = %i, Next Largest Multiple = %i\n",i,j,i + j - i % j);
     i = 996 ,j = 4 ;
-    printf ("i = %i, j = %i, Next Largest Multiple = %i\n\n",i,j,i + j - i % j);
+    printf ("i = %i, j = %i, Next Largest Multiple = %i\n",i,j,i + j - i % j);
 }
 
 int polynomial(void){
@@ -36,13 +23,19 @@ int polynomial(void){
     printf("3x^3 - 5x^2 + 6");
     double answer = 3*pow(x,3) - 5*pow(x,2) + 6;
     printf(" = %f\n", answer);
-//    printf(" = " + 3 * pow(3.0,3.0) - 5   )
 }
-
 
 int celsiusToF (void){
     float C; int F = 27;
 
     C = (F - 32) / 1.8 ;
     printf ("%i degrees F = %f degrees C\n",F ,C);
+}
+
+
+int main (void) {
+    celsiusToF();
+    polynomial();
+    printf("\n");
+    next_multiple();
 }
--- ch5.c
+++ ch5.c
@@ -1,11 +1,26 @@
 //
 // Created by Kevin Phillips on 1/5/16.
+// Following Programming in C by Stephen G Kochkan in preparation for CSCI E-28
 //
 
 
 #include <stdio.h>
 #include <math.h>
+/*
+ * This method prints out every 5th triangle number up to the for loop body limit of 50
+ */
+int question3(void){
+    int triangularNumber, i = 0;
+
+    printf("Number | triangularNumber\n");
+    printf("-------------------------\n");
+    for (int i = 5; i <= 50; i+=5) {
+        triangularNumber = i*(i + 1) / 2;
+        printf("#%2i            %i\n",i,triangularNumber);
+    }
+
+}
 
 int main(void) {
-
+    question3();
 }
